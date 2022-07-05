6415fae6e387a055ece1c4a9058d1b0e1ad58709
Pranav9Raj <75106143+Pranav9Raj@users.noreply.github.com>
2022-07-05 13:31:11 +0530

    Update price_tracker.py

--- price_tracker.py
+++ price_tracker.py
@@ -6,12 +6,18 @@ import smtplib
 
 
 
-
+#url of the product
 URL = 'https://www.amazon.in/OnePlus-Nord-Lite-128GB-Storage/dp/B09WQYFLRX/?_encoding=UTF8&pd_rd_w=jBUhF&content-id=amzn1.sym.e0e8ce89-ede3-4c51-b6ad-44989efc8536&pf_rd_p=e0e8ce89-ede3-4c51-b6ad-44989efc8536&pf_rd_r=S944Q3F2AQ1NR8MW31Q7&pd_rd_wg=pkcYR&pd_rd_r=592dac07-da0b-4261-829f-337424b722c2&ref_=pd_gw_ci_mcx_mr_hp_d'
+#just google my user agent to get thsi
 myheader = {"User-Agent" : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
 
+
+#fundtion to check the price of the product
 def price_check():
+    #request to get the html content of the page
     webpage = requests.get(URL, headers=myheader)
+    
+    #make a tree like structre using a parser to make search easy
     soup = BeautifulSoup(webpage.content, 'html.parser')
 
     title = soup.find(id='productTitle').get_text()
@@ -20,13 +26,14 @@ def price_check():
     price = price.replace(".", "")
     floatprice = float(price[0:6])
 
+    #strip to remove extra spaces
     print(title.strip())
     print(floatprice)
     if(floatprice < 17000):
         notify()
 
 
-
+# function to send the mail
 def notify():
     server=smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
