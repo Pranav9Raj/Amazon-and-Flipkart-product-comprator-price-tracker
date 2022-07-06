2ebaf4209a02638594551ff3f331c3f019c73b91
mayurrandive <75516478+mayurrandive@users.noreply.github.com>
2022-07-06 15:21:49 +0530

    Update price_tracker.py

--- price_tracker.py
+++ price_tracker.py
@@ -1,67 +1,87 @@
 
 import requests
-from bs4 import BeautifulSoup
-import time
-import smtplib
+import smtplib                     #simple mail transfer protocol
+from bs4 import BeautifulSoup      #for web scraping
+import time 
 
 
+# Put the URL of product of which you need to keep track of
 
-#url of the product
-URL = 'https://www.amazon.in/OnePlus-Nord-Lite-128GB-Storage/dp/B09WQYFLRX/?_encoding=UTF8&pd_rd_w=jBUhF&content-id=amzn1.sym.e0e8ce89-ede3-4c51-b6ad-44989efc8536&pf_rd_p=e0e8ce89-ede3-4c51-b6ad-44989efc8536&pf_rd_r=S944Q3F2AQ1NR8MW31Q7&pd_rd_wg=pkcYR&pd_rd_r=592dac07-da0b-4261-829f-337424b722c2&ref_=pd_gw_ci_mcx_mr_hp_d'
-#just google my user agent to get thsi
-myheader = {"User-Agent" : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
+# Amazon link
+product_URLa = "https://www.amazon.in/Apple-MacBook-Chip-13-inch-256GB/dp/B08N5XSG8Z/ref=lp_10559548031_1_1"
+# Flipkart link
+product_URLf = "https://www.flipkart.com/apple-2020-macbook-air-m1-8-gb-256-gb-ssd-mac-os-big-sur-mgn63hn-a/p/itmde54f026889ce?pid=COMFXEKMGNHZYFH9&lid=LSTCOMFXEKMGNHZYFH9P56X45&marketplace=FLIPKART&q=macbook+air+laptop+apple+m1+chip&store=6bo&srno=s_1_3&otracker=AS_Query_OrganicAutoSuggest_1_33_na_na_ps&otracker1=AS_Query_OrganicAutoSuggest_1_33_na_na_ps&fm=search-autosuggest&iid=8e373a6c-e452-401e-9fab-51c15945247e.COMFXEKMGNHZYFH9.SEARCH&ppt=sp&ppn=sp&ssid=oz7x8p4shs0000001657087829144&qH=c181b31cee16abe4"
+
+# Define header and pass your user agent info to header
+Header = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
+
+threshold = 92000.0      #threshold value to set for product 
+
+def mail(a: float, f: float):
+
+    Setserver=smtplib.SMTP('smtp.gmail.com',587)    #establish connection gmail and our server
+    Setserver.ehlo()           
+    Setserver.starttls()                            #encrpts the code
+    Setserver.ehlo()
+
+    Setserver.login('senders email', 'password')
+
+    Subject ='Price Got Dropped!!!'
+
+    Body_Amazon = 'Hurry Up!!! Check it out.... The Prices are Down - https://www.amazon.in/Apple-MacBook-Chip-13-inch-256GB/dp/B08N5XSG8Z/ref=lp_10559548031_1_1 '
+    Body_Flipkart = 'Hurry Up!!! Check it out.... The Prices are Down - https://www.flipkart.com/apple-2020-macbook-air-m1-8-gb-256-gb-ssd-mac-os-big-sur-mgn63hn-a/p/itmde54f026889ce?pid=COMFXEKMGNHZYFH9&lid=LSTCOMFXEKMGNHZYFH9P56X45&marketplace=FLIPKART&q=macbook+air+laptop+apple+m1+chip&store=6bo&srno=s_1_3&otracker=AS_Query_OrganicAutoSuggest_1_33_na_na_ps&otracker1=AS_Query_OrganicAutoSuggest_1_33_na_na_ps&fm=search-autosuggest&iid=8e373a6c-e452-401e-9fab-51c15945247e.COMFXEKMGNHZYFH9.SEARCH&ppt=sp&ppn=sp&ssid=oz7x8p4shs0000001657087829144&qH=c181b31cee16abe4 '
+
+    if(f>a):
+        message = f"Subject: {Subject}\n\n{Body_Amazon}"
+
+    else:
+        message = f"Subject: {Subject}\n\n{Body_Flipkart}"
+
+    Setserver.sendmail('Senders Email','Receivers Email', message)
+    print('Email is sent!!!')
+
+    Setserver.quit()
 
 
-#fundtion to check the price of the product
-def price_check():
-    #request to get the html content of the page
-    webpage = requests.get(URL, headers=myheader)
+def check_price():
+        
+    #Here we create a response object 'page' which will store the request-response.
+    pagea = requests.get(product_URLa, headers=Header)
+    pagef = requests.get(product_URLf, headers=Header)
+
+    #parse the data from HTML
+    soupa = BeautifulSoup(pagea.content, 'html.parser')
+    soupf = BeautifulSoup(pagef.content, 'html.parser')
+
+    #Get the name and price of product from amazon website using soup.find()
+    product_namea = soupa.find(id='productTitle').get_text()      #to get necessary text and not extra info
+    product_namef = soupf.find(class_='B_NuCI').get_text()
+
+    present_pricea = soupa.find(class_="a-price-whole").get_text()
+    present_pricef = soupf.find(class_="_30jeq3 _16Jk6d").get_text()
+
+    present_pricea = present_pricea.replace(",", "")  #removes the ',' from presenr_price string
+    present_pricea = present_pricea[0:5]              #extract the required number of characters from string
+    present_pricea = float(present_pricea[0:5])       #returns floating point from string
+
+    present_pricef = present_pricef.replace(",", "")  #removes the ',' from presenr_price string
+    present_pricef = present_pricef.replace("₹", "")  #removes the '₹' from presenr_price string
+    present_pricef = present_pricef[0:5]              #extract the required number of characters from string
+    present_pricef = float(present_pricef[0:5])       #returns floating point from string
+
+    if(int(present_pricea) < int(threshold) or int(present_pricef) < int(threshold) ):    
+        if(int(present_pricef) > int(present_pricea)):
+            print(present_pricea)  
+            print(product_namea.strip())   # strips out extra spaces
+            mail(present_pricea, present_pricef)
+
+        else:
+            print(present_pricef)  
+            print(product_namef.strip())   # strips out extra spaces
+            mail(present_pricea, present_pricef)
     
-    #make a tree like structre using a parser to make search easy
-    soup = BeautifulSoup(webpage.content, 'html.parser')
-
-    title = soup.find(id='productTitle').get_text()
-    price = soup.find(class_="a-price-whole").get_text()#here we search using class_ as class is a reserved word in python
-    price = price.replace(",", "")
-    price = price.replace(".", "")
-    floatprice = float(price[0:6])
-
-    #strip to remove extra spaces
-    print(title.strip())
-    print(floatprice)
-    if(floatprice < 17000):
-        notify()
-
-
-# function to send the mail
-def notify():
-    server=smtplib.SMTP('smtp.gmail.com',587)
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
-
-    server.login('senders mail id@gmail.com', 'pasword')
-
-    subject = 'Buy it Now!!'
-    body = 'the price of the product you were checking fell down!! BUY IT!! : https://www.amazon.in/OnePlus-Nord-Lite-128GB-Storage/dp/B09WQYFLRX/?_encoding=UTF8&pd_rd_w=jBUhF&content-id=amzn1.sym.e0e8ce89-ede3-4c51-b6ad-44989efc8536&pf_rd_p=e0e8ce89-ede3-4c51-b6ad-44989efc8536&pf_rd_r=S944Q3F2AQ1NR8MW31Q7&pd_rd_wg=pkcYR&pd_rd_r=592dac07-da0b-4261-829f-337424b722c2&ref_=pd_gw_ci_mcx_mr_hp_d'
-
-    msg = f"Subject: {subject}\n\n{body}"
-
-    server.sendmail(
-        'senders mail id',
-        'recivers mail id',
-        msg
-    )
-    print('Email is sent!!')
-
-    server.quit()
-
-
-
-
-while(True):
-    price_check()
-    time.sleep(60*60*2)
-
+ while(True)
+    check_price()         #Function call
+    time.sleep(60*60*2)   #Check eveys 2hrs
 
 
